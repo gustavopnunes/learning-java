4613004110720c7cfc020d7c7a6cf7697a61c47d
Gustavo P. Nunes <gustavo-pradogt@hotmail.com>
2022-03-03 02:19:00 -0300

    file reader

---
+++ projects/FileReader/src/File_Reader.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class File_Reader {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("file.txt");
+            BufferedReader br = new BufferedReader(reader);
+            String name = "";
+            do {
+                    name = br.readLine();
+                    if (name != null) {
+                        System.out.println(name);
+                    }
+
+            } while (name != null);`
+
+        }
+        catch(Exception ex) {
+            System.out.println("deu ruim");
+        }
+    }
+}
